d9418915aed1914c016b9b2ed98fb5d8bf7ad992
shamugia <m216126@usna.edu>
2021-02-26 15:06:22 -0800

    astar works?

--- aStar.py
+++ aStar.py
@@ -21,60 +21,60 @@ def aStar(board, heuristics):
     open_list.append(current_board)
 
     while len(open_list)>0:
-        
 
         current_board = open_list[0]
         current_board_index = 0
 
         for index, item in enumerate(open_list):
-            if current_board.f > item.f:
+            if item in closed_list:
+                continue
+            elif current_board.f > item.f:
                 current_board = item
                 current_board_index = index
+                board = current_board
 
-        open_list.pop(current_board_index)
         closed_list.append(current_board)
-
+        open_list.pop(current_board_index)
+        
 
         #Check if we are at the goal state
         if board.__eq__(goal):
-            # print(board.b)
-            # print('\n')
             print("Found Goal")
+
             current = current_board
 
             while True: #while not root
-                path.append(current.lb)
                 try:
+                    parent = current.parent
+                    path.append(parent.lb)
                     current = current.parent
                 except:
+                    # Return path and number of expanded nodes
                     return path[::-1], number_expanded
                         
-            # Return path and number of expanded nodes
-            
-
+           
         #Generating Children Nodes
         children = board.generateMoves()
+        print('\n')
         print("Printing Children Nodes")
         print(children)
-        print('\n')
 
         #Iterating thru the children nodes
         for move in children:
             print("Printing Individual Moves and new states")
-            # print(move)
             
             #making copies of the board before making the moves
             new_state = copy.deepcopy(board)
-            new_state.parent = board.lb
+            new_state.parent = current_board
+            print("Parent: ",new_state.parent)
 
             new_state.makeMove(move)
-            print(new_state.b)
+            print("New State: ", new_state.b)
 
             #If already seen the child or child with worse score
             # if (new_state.b in closed_list) and (new_state.g < current_board.g):
             #     break
             
-        
             #Heuristic. f(n) = g(n) + h(n)
             new_state.g = current_board.g + 1
             new_state.h = heuristics(new_state.b)
@@ -84,15 +84,17 @@ def aStar(board, heuristics):
             print("G cost: ", new_state.g)
             print("H cost: ", new_state.h)
             print("F cost: ", new_state.f)
-            print('\n')
 
-
+            if (new_state in closed_list):
+                if (new_state.f < closed_list[closed_list.index(new_state)].f):
+                    print("already in closed but update fcost")
+                    closed_list[closed_list.index(new_state)].f = new_state.f
+                else:
+                    print("Already in closed with better cost")
+                    continue
+            
             open_list.append(new_state)
 
-            if new_state not in closed_list:
-                closed_list.append(new_state)
-
-            
         number_expanded += 1 
 
 
--- idaStar.py
+++ idaStar.py
@@ -1,6 +1,44 @@
-import random
+from queue import PriorityQueue
+from Board import *
+from Board import manhattanDistance
+from Board import misplacedTiles
+import copy
 
-import sys
+def idaStar(board, heuristics):
+    limit = 0
+    done == False
+    number_expanded = 0
+    goal = [['b', 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
 
-def idaStar():
-    print('test')
+    print(str(board.b))
+
+    current_board = board
+    
+    # #Defining CLOSED and OPEN
+    open_list = []
+    closed_list = []
+    path = []
+
+
+
+    while not done:
+        if current_board.f <= limit:
+            
+            #Checking if goal state. If goal return path and # expandend nodes
+            if board.__eq__(goal):
+                print("Found Goal")
+
+                current = current_board
+
+                while True: #while not root
+                    try:
+                        parent = current.parent
+                        path.append(parent.lb)
+                        current = current.parent
+                    except:
+                        # Return path and number of expanded nodes
+                        return path[::-1], number_expanded
+            
+            
+            
+    
